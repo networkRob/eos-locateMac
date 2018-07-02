6cd7de3906102c8e8ea614c41c1710840c967961
Rob Martin <robmartin@arista.com>
2018-07-02 16:20:46 -0500

    Added initial functionality to check remote switches

--- locateMac.py
+++ locateMac.py
@@ -25,10 +25,10 @@ dict_all_mac_matches = {}
 
 switch_username = 'arista'
 switch_password = 'arista'
-switch_connections = []
+checked_switches = []
 
 #==========================================
-#Begin Class Declaration
+# Begin Class Declaration
 #==========================================
 
 class SwitchCon:
@@ -60,10 +60,18 @@ class SwitchCon:
                 l_base = lldp_results[r1]['lldpNeighborInfo'][0]
                 dict_lldp[r1] = {'neighbor':l_base['systemName'],'ip':l_base['managementAddresses'][0]['address'],'remote':l_base['neighborInterfaceInfo']['interfaceId'],'bridge':l_base['systemCapabilities']['bridge'],'router':l_base['systemCapabilities']['router']}
         return(dict_lldp)
+    def get_lldp_br(self):
+        "Returns a dictionary of LLDP neighbors that are bridges and routers"
+        tmp_dict = {}
+        for r1 in self.lldp_neighbors:
+            if self.lldp_neighbors[r1]['bridge'] and self.lldp_neighbors[r1]['router']:
+                tmp_dict[r1] = self.lldp_neighbors[r1]
+        return(tmp_dict)
+            
         
 
 #==========================================
-#End Class Declaration
+# End Class Declaration
 #==========================================
 # Begin Function Declarations
 #==========================================
@@ -86,15 +94,45 @@ def format_MAC(mac_string):
     elif mac_len >= 2:
         new_mac_search = "%s%s" %tuple(tmp_mac)
     return(new_mac_search)
-        
-def print_output(hostname,intf,mac):
+
+
+def print_output(data):
     print('MAC Address\t\tHostname\tInterface')
-    print('%s\t%s\t\t%s'%(mac,hostname,intf))
+    print('%s\t%s\t\t%s'%(data))
+
+def query_switch(switch_con,new_mac_search):
+    response_current_switch = switch_con.run_commands(['show mac address-table | include %s'%new_mac_search])
+    #Check to see if a response was returned for the 'show mac address-table command'
+    if response_current_switch[0]:
+        mac_response = response_current_switch[0]['unicastTable']['tableEntries']
+        #Iterate through all unicast table entries
+        for r1 in mac_response:
+            if new_mac_search in r1['macAddress']:
+                if 'Port-Channel' in r1['interface']:
+                    port_details = switch_con.run_commands(['show interfaces %s'%r1['interface']])
+                    if port_details[0]: #Verify a response was generated
+                        intf_members = [] #Temp variable to contain interfaces within a port-channel
+                        for r2 in port_details[0]['interfaces'][r1['interface']]['memberInterfaces']:
+                            if 'Peer' not in r2:
+                                intf_members.append(r2)
+                        switch_con.add_mac(r1['macAddress'],intf_members,r1['vlanId'])
+                else:
+                    switch_con.add_mac(r1['macAddress'],[r1['interface']],r1['vlanId'])
+    
+
+#==========================================
+# End Function Declarations
+#==========================================
+# Begin Main Function
+#==========================================
+
 
 def main(mac_to_search):
     "Main script to get started"
     #Status Variable
     SEARCH_STATUS = False
+    search_count = 0 #counting variable to prevent forever loop
+    search_devices = [] # List containing IPs to remote query
     #Reformat MAC search string
     if '.' in mac_to_search:
         new_mac_search = format_MAC(mac_to_search)
@@ -103,36 +141,49 @@ def main(mac_to_search):
     else:
         new_mac_search = format_MAC(mac_to_search)
     current_switch = SwitchCon('localhost',switch_username,switch_password)
-    switch_connections.append(current_switch)
-    response_current_switch = current_switch.run_commands(['show mac address-table | include %s'%new_mac_search])
-    #Check to see if a response was returned for the 'show mac address-table command'
-    if response_current_switch[0]:
-        mac_response = response_current_switch[0]['unicastTable']['tableEntries']
-        #Iterate through all unicast table entries
-        for r1 in mac_response:
-            if new_mac_search in r1['macAddress']:
-                if 'Port-Channel' in r1['interface']:
-                    port_details = current_switch.run_commands(['show interfaces %s'%r1['interface']])
-                    if port_details[0]: #Verify a response was generated
-                        intf_members = [] #Temp variable to contain interfaces within a port-channel
-                        for r2 in port_details[0]['interfaces'][r1['interface']]['memberInterfaces']:
-                            if 'Peer' not in r2:
-                                intf_members.append(r2)
-                        current_switch.add_mac(r1['macAddress'],intf_members,r1['vlanId'])
-                else:
-                    current_switch.add_mac(r1['macAddress'],[r1['interface']],r1['vlanId'])
-    pp(current_switch.mac_entry)
-    pp(current_switch.lldp_neighbors)
+    query_switch(current_switch,new_mac_search)
     #Iterate through mac entries found
     for r1 in current_switch.mac_entry:
         for r2 in current_switch.mac_entry[r1]['interface']:
             #Evaluate if the interface the MAC was found on has a LLDP neighbor entry
             if r2 not in current_switch.lldp_neighbors: 
-                print_output(current_switch.hostname,r2,r1)
+                SEARCH_STATUS = True
+                mac_switch_result = (current_switch.hostname,r2,r1)
+            else:
+                for r3 in current_switch.lldp_neighbors:
+                    search_devices.append(current_switch.lldp_neighbors[r3]['ip'])
+    while not SEARCH_STATUS:
+        if search_count >= 20:
+            print('Not found')
+            break
+        search_count += 1
+        for r1 in search_devices:
+            if r1 not in checked_switches:
+                checked_switches.append(r1)
+                remote_switch = SwitchCon(r1,switch_username,switch_password)
+                query_switch(remote_switch,new_mac_search)
+                for r2 in remote_switch.mac_entry:
+                    for r3 in remote_switch.mac_entry[r2]['interface']:
+                        #Evaluate if the interface the MAC was found on has a LLDP neighbor entry
+                        if r3 not in remote_switch.lldp_neighbors: 
+                            SEARCH_STATUS = True
+                            mac_switch_result = (remote_switch.hostname,r3,r2)
+                        else:
+                            for r4 in remote_switch.lldp_neighbors:
+                                if remote_switch.lldp_neighbors[r4]['ip'] not in checked_switches and remote_switch.lldp_neighbors[r4]['ip'] not in search_devices:
+                                    search_devices.append(remote_switch.lldp_neighbors[r4]['ip'])
+    if SEARCH_STATUS:
+        print_output(mac_switch_result)
+                    
+
+                        
                 
     #TODO: Check to see if a match was found on local switch end and report
     #TODO: If match found on remote switch, query remote switch, if round end and report it.  If on remote, repeat queries
 
+#==========================================
+# End Main Function
+#==========================================
 
 if __name__ == '__main__':
     if len(argv) > 1:
